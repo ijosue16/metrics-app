4b0328901b971aba0cfaaef5b7560307fdfa38eb
ijosue16 <iradukundajosue16@gmail.com>
2023-02-17 21:20:15 +0200

    fix linters

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,7 @@
 import { applyMiddleware, combineReducers, legacy_createStore as createStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import { pickedCityReducer, weatherReducer } from './weather';
+
 const rootReducer = combineReducers({
   Weather: weatherReducer,
   Pickedcity: pickedCityReducer,
