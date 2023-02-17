643f3420b8c01e00c3995f1601da0b835b76c1ca
ijosue16 <iradukundajosue16@gmail.com>
2023-02-17 21:07:14 +0200

    setup store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,9 @@
+import { applyMiddleware, combineReducers, legacy_createStore as createStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import { pickedCityReducer, weatherReducer } from './weather';
+const rootReducer = combineReducers({
+  Weather: weatherReducer,
+  Pickedcity: pickedCityReducer,
+});
+const store = createStore(rootReducer, applyMiddleware(thunk));
+export default store;
